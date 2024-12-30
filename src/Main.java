07a211f444bbc1fccc1080d295b11ab4bae4889a
joanavilaa <joan.avila@students.salle.url.edu>
2024-12-30 22:13:39 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
 
-        controller.run(); // Run the program
+        controller.run(); // Run the program.
     }
 }
